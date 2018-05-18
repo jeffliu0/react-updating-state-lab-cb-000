bb20b7ae0aa9f317e74d7afbf8ca83b141b17482
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-05-18 21:38:49 +0000

    automatically backed up by learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,14 @@
 // Code DigitalClicker Component Here
+
+export default class DigitalClicker extends React.Component{
+  constructor(){
+    super();
+
+    this.state={
+      timesClicked = 0
+    }
+  }
+  handleClick = (event)=>{
+
+  }
+}
